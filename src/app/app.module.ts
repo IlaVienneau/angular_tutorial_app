6144f02919d819aa3eb938d9ad43433e3654bcd5
IlaVienneau <ivienneau3@gatech.edu>
2020-06-17 16:14:02 -0400

    build events-list data-bound component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent
+    EventsAppComponent,
+    EventsListComponent
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
